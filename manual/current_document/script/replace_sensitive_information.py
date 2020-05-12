88d397b711e717ca85203aa8cc21c762f90d2052
yuwei01 <yuwei01@inspur.com>
2020-05-12 15:32:12 +0800

    “替换部分敏感信息#75”

--- manual/current_document/script/replace_sensitive_information.py
+++ manual/current_document/script/replace_sensitive_information.py
@@ -3,7 +3,7 @@
 import sys
 import os
 
-replace_list = [('gpactivestandby', 'hdbactivestandby'),
+replace_list = [('gpactivatestandby', 'hdbactivatestandby'),
                 ('gpaddmirrors', 'hdbaddmirrors'),
                 ('gpbackup', 'hdbbackup'),
                 ('gpcheckcat', 'hdbcheckcat'),
@@ -18,7 +18,7 @@ replace_list = [('gpactivestandby', 'hdbactivestandby'),
                 ('gpload.bat', 'hdbload.bat'),
                 ('gpload.py', 'hdbload.py'),
                 ('gplogfilter', 'hdblogfilter'),
-                ('gpmapredude', 'hdbmapredude'),
+                ('gpmapreduce', 'hdbmapreduce'),
                 ('gpperfmon_install', 'hdbperfmon_install'),
                 ('gpperfmoncat.sh', 'hdbperfmoncat.sh'),
                 ('gpmmon', 'hdbmmon'), ('gpsmon', 'hdbsmon'),
@@ -38,8 +38,14 @@ replace_list = [('gpactivestandby', 'hdbactivestandby'),
                 ('gpcheckresgroupimpl','hdbcheckresgroupimpl'),
                 ('Greenplum','inHybrid'),
                 ('GPHOME','HDBHOME'),
-                ('gpadmin','hdbadmin')]
-
+                ('gpadmin','hdbadmin'),
+                ('pivotal','inspur'),
+                ('Pivotal','inspur'),
+                ('Gpload','gpload'),
+                ('Gpfdist','gpfdist'),
+                ('GPDB','HDB'),
+                ('gpdb','hdb'),
+                ('gpmaster','hdbmaster')]
 def find_all_utf8_html_list(path, file_list):
     for root, dirs, files in os.walk(path):
         for file_name in files:
@@ -55,15 +61,26 @@ def exec_replace_command(file_list):
         for rep_data in replace_list:
             if len(rep_data) == 2:
                 command = 'sed -i s#' + rep_data[0] + '#' + rep_data[1] + '#g ' + source_file
+                #command = 'grep -i ' + rep_data[0] + " " + source_file + " >> a.txt"
+                print command
                 os.system(command)
 
 
 if __name__ == '__main__':
     try:
-        path = os.path.abspath(sys.argv[0] + '/../../document/6-0/')
-        file_list = []
-        find_all_utf8_html_list(path, file_list)
-        exec_replace_command(file_list)
+        paths = ['/home/yw/data/innergpcode/20200507/hdb/manual/current_document/admin_guide/6-0/',
+                 '/home/yw/data/innergpcode/20200507/hdb/manual/current_document/best_practices/6-0/',
+                 '/home/yw/data/innergpcode/20200507/hdb/manual/current_document/pxf/6-0/',
+                 '/home/yw/data/innergpcode/20200507/hdb/manual/current_document/ref_guide/6-0/',
+                 '/home/yw/data/innergpcode/20200507/hdb/manual/current_document/security_guide/6-0/',
+                 '/home/yw/data/innergpcode/20200507/hdb/manual/current_document/utility_guide/6-0/']
+        for path in paths:
+        #path = os.path.abspath(sys.argv[0] + '/../../document/6-0/')
+            print path
+            file_list = []
+            find_all_utf8_html_list(path, file_list)
+            print file_list
+            exec_replace_command(file_list)
     except BaseException as e:
         print 'exec replace error,', e
         exit(-1)
